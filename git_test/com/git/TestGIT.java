e24b9c59bfcfaac86e77e2640d3aab3b0fa00503
zhang <zemmeleta@gmail.com>
2015-10-28 16:53:06 +0800

    11

--- git_test/com/git/TestGIT.java
+++ git_test/com/git/TestGIT.java
@@ -26,6 +26,7 @@ public class TestGIT extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().println("this is from git resp");
+		response.getWriter().println("this is from git resp2");
 		response.getWriter().flush();
 		
 	}
